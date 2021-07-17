73d4a91f56759e295da889abcd7564206ae70dbf
PabloViniegra <pviniegra@imagar.com>
2021-07-17 11:27:03 +0200

    Agregadas más validaciones

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ path = '../FTP'
 excelfiles = os.listdir(path)
 patternToExcel = '.+\.(xlsx|xls|xlsm)'
 #Cuando ejecutemos el script tiene que venir como argumento el nombre de la campaña para saber que validacion hacer. Con sys.argv recogemos una lista de los argumentos donde el [0] es el propio nombre del script
-#if sys.argv == 2:
+if sys.argv == 2:
 
-    #if 'Premiamos_Tu_Confianza_Dental_Pre' == sys.argv[1]:'
-        #Instanciamos el parser y ejecutamos su validación
-    #elif 'Ventajas_Basico' == sys.argv[1]:           La siguiente campaña
-#else:
-#print("Los argumentos no son correctos")
+    if 'Premiamos_Tu_Confianza_Dental_Pre' == sys.argv[1]:
+        #Instanciamos el parser1 y ejecutamos su validación
+        parser1 = validations.PremiamosTuConfianzaDentalPre(excelfiles, patternToExcel)
+        check = parser1.executeValidations()
+        if check:
+            print("Las validaciones han sido correctas")
+            open('../OK.txt', 'w')
+        else:
+            print("No han pasado las validaciones")
+    elif 'Ventajas_Basico' == sys.argv[1]:
+        #Instancia de parser de la campaña 2
+        parser2 = validations.VentajasBasico(excelfiles, patternToExcel)
+        check = parser2.executeValidations()
+        if check:
+            print("Las validaciones han sido correctas")
+            open('../OK.txt', 'w')
+        else:
+            print("No han pasado las validaciones")
+    elif 'Ventajas_Plena' == sys.argv[1]:
+        #Instancia de parser de la campaña 3
+        pass
+else:
+    print("Los argumentos no son correctos")
 
 
 #TODO: desarrollar y probar mas parsers
-parser1 = validations.PremiamosTuConfianzaDentalPre(excelfiles, patternToExcel)
-check = parser1.validationCommon()
-if check:
-    print("Las validaciones han sido correctas")
-    open('../OK.txt', 'w')
-else:
-    print("No han pasado las validaciones")
--- validations.py
+++ validations.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 from openpyxl import load_workbook
+pathToFiles = '../FTP/'
 
 class PremiamosTuConfianzaDentalPre():
     excelfiles = []
@@ -9,21 +10,21 @@ class PremiamosTuConfianzaDentalPre():
         excelfiles = self.excelfiles
         patternToExcel = self.patternToExcel
 
-    def validationCommon(self):
+    def executeValidations(self):
         checkNIFandCard = True
         for file in self.excelfiles:
             if re.match(self.patternToExcel, file, re.M | re.I):
-                df = pd.read_excel('ExcelFiles/' + file)
+                df = pd.read_excel(pathToFiles + file)
                 line_number = 2
                 for index, row in df.iterrows():
                     # print(row)
                     # print(index)
                     if pd.isna(row['IDIOMA']):
                         print("La fila " + str(line_number) + " no tiene idioma en " + file)
-                        workbook = load_workbook(filename='ExcelFiles/' + file)
+                        workbook = load_workbook(filename=pathToFiles + file)
                         sheet = workbook.active
                         sheet["C" + str(line_number)] = "CAS"
-                        workbook.save('ExcelFiles/' + file)
+                        workbook.save(pathToFiles + file)
                     if pd.isna(row['N_TARJETA']) and pd.isna(row['NIF']):
                         print("La fila " + str(line_number) + " no tiene número de tarjeta ni NIF en " + file)
                         with open('../validations.txt', 'a') as f:
@@ -43,6 +44,102 @@ class VentajasBasico():
     def __init__(self, excelfiles, patternToExcel):
         excelfiles = self.excelfiles
         patternToExcel = self.patternToExcel
-    #TODO: validaciones de la Campaña 2
-    def validationCommon(self):
-        pass
+
+    def executeValidations(self):
+        global checkIfCardExist
+        for file in self.excelfiles:
+            checkIfCardExist = True
+            #Creamos la columna de TARGET para agregar los datos que se piden
+            workbook = load_workbook(filename=pathToFiles + file)
+            sheet = workbook.active
+            sheet["Z1"] = 'TARGET'
+            workbook.save(pathToFiles + file)
+            if re.match(self.patternToExcel, file, re.M | re.I):
+                df = pd.read_excel(pathToFiles + file)
+                line_number = 2
+                # validacion de si en target corresponde JOV o GRAL
+                if '_JOV_' in file:
+                    print("En el archivo " + file + " el target debe ser JOVEN")
+                    targetToAdd = 'JOVEN'
+                else:
+                    targetToAdd = 'GRAL'
+                    print("En el archivo " + file + " el target debe ser GRAL")
+                for index, row in df.iterrows():
+                    #validación del idioma
+                    if pd.isna(row['IDIOMA']):
+                        print("La fila " + str(line_number) + " no tiene idioma en " + file)
+                        workbook = load_workbook(filename=pathToFiles + file)
+                        sheet = workbook.active
+                        sheet["C" + str(line_number)] = "CAS"
+                        workbook.save('ExcelFiles/' + file)
+                    #validación de si el numero de tarjeta está en blanco
+                    if pd.isna(row['N_TARJETA']):
+                        print("La fila " + str(line_number) + " no tiene número de tarjeta en " + file)
+                        with open('../validations.txt', 'a') as f:
+                            f.write("La fila " + str(line_number) + " no tiene número de tarjeta en " + file + "\n")
+                        checkIfCardExist = False
+                    #validación de si el codigo de promo esta en blanco o no es el especificado
+                    if 'CADB1' != row['CODIGO_PROMO'] or pd.isna(row['CODIGO_PROMO']):
+                        print("La fila " + str(line_number) + " no tiene el codigo de promoción correcto en " + file + ". Se establece CADB1 por defecto")
+                        workbook = load_workbook(filename=pathToFiles + file)
+                        sheet = workbook.active
+                        sheet["Y" + str(line_number)] = 'CADB1'
+                        workbook.save(pathToFiles + file)
+                    #Escribimos en la columna TARGET su valor correspondiente
+                    workbook = load_workbook(filename=pathToFiles + file)
+                    sheet = workbook.active
+                    sheet["Z" + str(line_number)] = targetToAdd
+                    workbook.save(pathToFiles + file)
+                    line_number += 1
+            else:
+                raise Exception(file + " is not an Excel File !")
+        return checkIfCardExist
+
+class VentajasPlena():
+    excelfiles = []
+    patternToExcel = ""
+
+    def __init__(self, excelfiles, patternToExcel):
+        excelfiles = self.excelfiles
+        patternToExcel = self.patternToExcel
+
+    def executeValidations(self):
+        for file in self.excelfiles:
+            checkIfCardExist = True
+            targetToAdd = 'GRAL'
+            workbook = load_workbook(filename=pathToFiles + file)
+            sheet = workbook.active
+            sheet["Z1"] = 'TARGET'
+            workbook.save(pathToFiles + file)
+            if re.match(self.patternToExcel, file, re.M | re.I):
+                df = pd.read_excel(pathToFiles + file)
+                line_number = 2
+                for index, row in df.iterrows():
+                    #validación del idioma
+                    if pd.isna(row['IDIOMA']):
+                        print("La fila " + str(line_number) + " no tiene idioma en " + file)
+                        workbook = load_workbook(filename=pathToFiles + file)
+                        sheet = workbook.active
+                        sheet["C" + str(line_number)] = "CAS"
+                        workbook.save(pathToFiles + file)
+                    # validación de si el numero de tarjeta está en blanco
+                    if pd.isna(row['N_TARJETA']):
+                        print("La fila " + str(line_number) + " no tiene número de tarjeta en " + file)
+                        with open('../validations.txt', 'a') as f:
+                            f.write("La fila " + str(line_number) + " no tiene número de tarjeta en " + file + "\n")
+                        checkIfCardExist = False
+                        # validación de si el codigo de promo esta en blanco o no es el especificado
+                    if 'CAD2' != row['CODIGO_PROMO'] or pd.isna(row['CODIGO_PROMO']):
+                        print("La fila " + str(line_number) + " no tiene el codigo de promoción correcto en " + file + ". Se establece CAD2 por defecto")
+                        workbook = load_workbook(filename=pathToFiles + file)
+                        sheet = workbook.active
+                        sheet["Y" + str(line_number)] = 'CAD2'
+                        workbook.save(pathToFiles + file)
+                    #Escribimos en la columna TARGET el valor especificado
+                    workbook = load_workbook(filename=pathToFiles + file)
+                    sheet = workbook.active
+                    sheet["Z" + str(line_number)] = targetToAdd
+                    workbook.save(pathToFiles + file)
+                    line_number += 1
+            else:
+                raise Exception(file + " is not an Excel File !")
